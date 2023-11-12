112ee6094814161ea3c138752ad9303d59f8816a
ikuma-t <tadokorodev@gmail.com>
2023-11-12 11:15:29 +0900

    commit

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,9 +5,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe '#sample' do
     it 'first test' do
-      debugger # VSCodeのデバッガでは止まらない
-      expect(Post.new.sample).to eq('SAMPLE') # VSCodeのブレークポイントで止まる
-      binding.b # VSCodeのデバッガでも止まる
+      post = Post.new
+      expect(post.sample).to eq('SAMPLE') # VSCodeのブレークポイントで止まる
     end
+  end
+
+  describe 'associations' do
+    it { should have_many(:comments) }
   end
 end
